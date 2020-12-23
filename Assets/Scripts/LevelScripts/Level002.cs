c072bf54bd9214da86e28e25e59d00460d218d1f
Jade <jade_montague@yahoo.com>
2020-12-23 05:35:42 -0600

    Level Scripts

---
+++ Assets/Scripts/LevelScripts/Level002.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level002 : MonoBehaviour
+{
+    public GameObject fadeIn;
+
+    void Start()
+    {
+        RedirectToLevel.redirectToLevel = 5;
+        //Level Creation is made with the following script
+        RedirectToLevel.nextLevel = 6;
+        StartCoroutine(FadeInOff());
+    }
+
+    IEnumerator FadeInOff()
+    {
+        yield return new WaitForSeconds(1);
+        fadeIn.SetActive(false);
+    }
+
+}
